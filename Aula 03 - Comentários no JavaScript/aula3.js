f82e2c78c78130a3fe03e176bed96570458a9d31
samuel-franco <samuelfranko66@gmail.com>
2024-12-25 18:16:08 -0300

    adciona as aulas

---
+++ Aula 03 - Comentários no JavaScript/aula3.js
@@ -0,0 +1,11 @@
+
+// Aqui segue um comentário de uma linha usando duas barras
+//console.log('Olá meu nome é "Samuel". Estou aprendendo o JavaScript às', 10, 'da manhã'); 
+// console.log("Olá mundo");
+
+/* Aqui segue um comentário de mais de uma linha */
+
+console.log("Linha 0");
+console.log("Linha 1");
+console.log("Linha 2");
+console.log("Linha3");
